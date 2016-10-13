5e185b18b7b5e4a0606e976c1bcbd03289a12911
fenix-soft <fenix.forum.mail@gmail.com>
2016-10-13 11:43:34 +0200

    Update RAI1.sh
    modifica per usare mpv (istallato nel sistema e non custom compilato)

--- RAI1.sh
+++ RAI1.sh
@@ -8,7 +8,7 @@ sed -e 's/<\/url>//g' temp2.txt >temp.txt
 dos2unix temp.txt
 surce=`cat temp.txt`
 echo $surce
-./mpv --hls-bitrate=max "$surce"
+mpv --hls-bitrate=max "$surce"
 rm p.txt
 rm temp.txt
 rm temp2.txt
