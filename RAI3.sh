eb875e8e7a8c5213bc5c33320dff52a4629b0610
fenix-soft <fenix.forum.mail@gmail.com>
2021-10-05 14:13:23 +0200

    Add files via upload

--- RAI3.sh
+++ RAI3.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# V1.1 20210905 Public relase
 
 wget -d "http://mediapolis.rai.it/relinker/relinkerServlet.htm?cont=308709&output=45" -U "iphone" -O p.txt
 grep content  p.txt >temp.txt
@@ -8,7 +9,14 @@ sed -e 's/<\/url>//g' temp2.txt >temp.txt
 dos2unix temp.txt
 surce=`cat temp.txt`
 echo $surce
-mpv --hls-bitrate=max "$surce"
+
+#max bitrate video
+#mpv2 --hls-bitrate=max "$surce"
+
+#Video --vid=2 'bitrate 2137033' (h264)
+mpv --vid=2 "$surce"
+
+
 rm p.txt
 rm temp.txt
 rm temp2.txt
